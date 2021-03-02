b5b577cd9d6b163fd88550caf42ba1f7711bd290
Isaac Rodriguez <isaac.romo.1002@gmail.com>
2021-03-02 13:40:40 -0600

    Validando el formulario

--- js/app.js
+++ js/app.js
@@ -11,5 +11,30 @@ function eventListeners() {
 function agregarTweet(e) {
   e.preventDefault();
 
-  console.log("Agregando Tweet");
+  // textarea donde el usuario escribe
+  const tweet = document.querySelector("#tweet").value; // accedemos al valor
+
+  // Validacion
+  if (tweet === "") {
+    mostrarError("Un mensaje no puede ir vacio");
+    return;
+  }
+  console.log("agregando tweets");
+}
+
+// Mostrar mensaje de error
+
+function mostrarError(error) {
+  const mensajeError = document.createElement("p");
+  mensajeError.textContent = error;
+  mensajeError.classList.add("error");
+
+  //insertarlo en el contenido
+  const contenido = document.querySelector("#contenido");
+  contenido.appendChild(mensajeError);
+
+  // Despues de 3 seg se elimina el mensaje de error
+  setTimeout(() => {
+    mensajeError.remove();
+  }, 3000);
 }
